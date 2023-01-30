e8a40ec25c67f2b448832e493de4ec8c5c9d8d29
Meech <meech@dannys-mbp.attlocal.net>
2023-01-30 11:24:39 -0500

    crated a function to display right or wrong answers

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const question = document.getElementById('question');
 const answerChoice = Array.from(document.getElementsByClassName('answer-text'));
 
 let currentQuestion = {};
-let answer = true;
+let answer = false;
 let score = 0;
 let quesitonCount = 0;
 let remainingQuestions = [];
@@ -10,35 +10,43 @@ let remainingQuestions = [];
 let questions = [
     {
         question: "What are the data types supported by JavaScript?",
-        answer1: "boolean, null, string, symbol",
-        answer2: "Flex-box, array, variable, integers",
-        answer3: "symbol, object, number, margin",
-        answer4: "python, java, html, css ",
+        answers: [
+            "boolean, null, string, symbol",
+            "flex-box, array, variable, integers",
+            "symbol, object, number, margin",
+            "python, java, html, css ",
+        ],
         answer: 1
     },
     {
         question: "What does CSS stand for?",
-        answer1: "Cascading Style Ship",
-        answer2: "Cats Slip Slide",
-        answer3: "Cascading Style Sheet",
-        answer4: "None of the above",
+        answers: [
+            "Cascading Style Ship",
+            "Cats Slip Slide",
+            "Cascading Style Sheet",
+            "None of the above",
+        ],
         answer: 3
     },
     {
         question: "What does HTML stand for?",
-        answer1: "Hyper Tom Moves LOgs",
-        answer2: "Hyper Text Markup Language",
-        ansewr3: "Home Tool Markup Language",
-        answer4: "Hyper and Text Markup Language",
+        answers: [
+            "Hyper Tom Moves Logs",
+            "Hyper Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper and Text Markup Language",
+        ],
         answer: 2
     },
 
     {
         question: "How can you  make a numbered list?",
-        answer1: "<ul>",
-        answer2: "<li>",
-        answer3: "<dl>",
-        answer4: "<ol>",
+        answers: [
+            "<ul>",
+            "<li>",
+            "<dl>",
+            "<ol>",
+        ],
         answer: 4
     }
 
@@ -52,25 +60,50 @@ const questionMax = 4;
 startGame = () => {
     questionCount = 0;
     score = 0;
-    remainingQuestions = [... questions];
+    remainingQuestions = [...questions];
     getNewQuestion();
 };
 
 getNewQuestion = () => {
 
     questionCount++;
-    const questionIndex = Math.floor(Math.random() * remainingQuestions.length);  
+    const questionIndex = Math.floor(Math.random() * remainingQuestions.length);
     currentQuestion = remainingQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
+     for (let i = 0; i < 4; i++) {
+         var answerEl = document.getElementById(`answer-${[i]}`);
+        answerEl.textContent = currentQuestion.answers[i];
+    }
 
-    answerChoice.forEach( answerChoice => {
-        const number = answerChoice.dataset['number'];
-        answerChoice.innerText = currentQuestion['answerChoice' + number];
-    });
+    // answerChoice.forEach(answerChoice => {
+    //     const number = answerChoice.dataset['number'];
+    //     answerChoice.innerText = currentQuestion['answerChoice' + number];
+    // });
 
     remainingQuestions.splice(questionIndex, 1);
+
 };
+
+function answerChecker(event) {
+    var answerChoice = event.target
+
+    if (!answerChoice.matches(".answer-choice")) {
+        console.log("not a answer")
+        return;
+    }
+
+    if (answerChoice.value !== "target your answer here") {
+        console.log("wrong answer")
+    } else {
+        console.log("right answer")
+    }
+
+}
+
+var answerContainer = document.querySelector('.container');
+
+answerContainer.onclick = answerChecker;
 
 
 startGame();
